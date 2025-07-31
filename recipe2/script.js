c197618efcbfbdd7635a0f02c809fb364e1df964
Arpit pandey <arpitpandey93356@gmail.com>
2025-07-31 14:11:36 +0530

    code updated

--- recipe2/script.js
+++ recipe2/script.js
@@ -25,3 +25,29 @@ favButtons.forEach(btn => {
     btn.classList.toggle('favorited');
   });
 });
+
+// Read more button
+
+document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll(".read-more-btn");
+  
+    buttons.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        const card = btn.closest(".recipe-card");
+        const dots = card.querySelector(".dots");
+        const moreText = card.querySelector(".more");
+  
+        if (dots.style.display === "none") {
+          dots.style.display = "inline";
+          moreText.style.display = "none";
+          btn.textContent = "Read more";
+        } else {
+          dots.style.display = "none";
+          moreText.style.display = "inline";
+          btn.textContent = "Read less";
+        }
+      });
+    });
+  });
+  
+  
